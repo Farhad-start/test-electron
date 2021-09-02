7e74f6a7cd838fa1af51b565f9e7b5e1ad5826fa
Anton Fefilov <atomdev@icloud.com>
2021-09-02 12:39:06 +0400

    Scaffold project building infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'common.js',
+    path: path.resolve(__dirname, 'dist', 'js'),
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: '../css/style.css' })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Extract CSS into file
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
